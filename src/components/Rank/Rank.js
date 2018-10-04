88f8d922932cdd81436d02f98e2e38616ac22459
nic-ball <nicholasball147@gmail.com>
2018-10-04 23:33:14 +0100

    Fixed spacing and styling with prettier

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Rank = () => {
 	return (
 		<div>
-			<div className='white f3'>
-				{'Nic, your current rank is...'}
-			</div>
-			<div className='white f1'>
-				{'#5'}
-			</div>
+			<div className="white f3">{"Nic, your current rank is..."}</div>
+			<div className="white f1">{"#5"}</div>
 		</div>
 	);
-}
+};
 
 export default Rank;
